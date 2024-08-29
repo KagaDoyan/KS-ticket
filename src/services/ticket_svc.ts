6d14b3aad1c2d1c85f63185c095eba0bb54a8bf2
jack <phanoudeth@kokkok.la>
2024-08-29 08:26:05 +0700

    fix dubplicate data

--- src/services/ticket_svc.ts
+++ src/services/ticket_svc.ts
@@ -557,6 +557,7 @@ export const ticketSvc = {
             },
             include: {
                 shop: true,
+                engineer: true,
                 customer: true,
                 store_item: {
                     where: {
