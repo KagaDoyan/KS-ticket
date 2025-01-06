6436bdef2829c1bdece391ece6dc70a35aa2a8fe
jack <phanoudeth@kokkok.la>
2025-01-06 10:41:59 +0700

    new user role

--- src/services/user_svc.ts
+++ src/services/user_svc.ts
@@ -59,7 +59,7 @@ export const UserSvc = {
                 fullname: payload.fullname,
                 email: payload.email,
                 role: payload.role,
-                customer_id: payload.customer_id
+                customer_id: payload.role == "customer" ? payload.customer_id : null
             }
         });
         return user
